1eeb51abda789e1b93bd900e685acbe59a436949
abdurrahim220 <z.abdurrahim5@gmail.com>
2023-11-25 13:28:07 +0600

    hero banner done

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,29 @@ import { useEffect, useState } from "react";
 
 import "./App.css";
 import { fetchDataFromApi } from "./utils/api";
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration } from "./store/homeSlice";
 function App() {
-  const dispatch = useDispatch()
-  const {url}=useSelector((state)=>state.home)
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
   // console.log(url)
   useEffect(() => {
     fetchDataFromApi("/movie/popular").then((res) => {
       // console.log(res);
-      dispatch(getApiConfiguration(res))
+      const url = {
+        backdrop : res.images.secure_base_url + "original",
+        poster : res.images.secure_base_url + "original",
+        profile : res.images.secure_base_url + "original"
+      }
+      dispatch(getApiConfiguration(url));
     });
   }, []);
 
-  return <>
-  <div>
-    {url?.total_pages}
-  </div>
-  </>;
+  return (
+    <>
+      <div>{url?.total_pages}</div>
+    </>
+  );
 }
 
 export default App;
--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 import Footer from "../Shared/Footer/Footer";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchDataFromApi } from "../utils/api";
+import { getApiConfiguration } from "../store/homeSlice";
 
 const Main = () => {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  // console.log(url)
+  useEffect(() => {
+    fetchDataFromApi("/configuration").then((res) => {
+      // console.log(res);
+      const url = {
+        backdrop : res.images.secure_base_url + "original",
+        poster : res.images.secure_base_url + "original",
+        profile : res.images.secure_base_url + "original"
+      }
+      dispatch(getApiConfiguration(url));
+    });
+  }, []);
   return (
     <div>
       <Navbar />
