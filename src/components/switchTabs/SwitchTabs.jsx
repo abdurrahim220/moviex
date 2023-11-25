416b4998f8636b63a8167e81ace102a9a73c7d97
abdurrahim220 <z.abdurrahim5@gmail.com>
2023-11-26 00:15:16 +0600

    trending component & page added

--- src/components/switchTabs/SwitchTabs.jsx
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./switchTabs.scss";
 const SwitchTabs = ({ data, onTabChange }) => {
-    
+  const [selectTab, setSelectTab] = useState(0);
+  const [left, setLeft] = useState(0);
+  const activeTab = (tab,index) => {
+    setLeft(index * 100);
+    setTimeout(() => {
+      setSelectTab(index);
+    }, 300);
+    onTabChange(tab, index);
+  };
+
   return (
     <div className="switchingTabs">
       <div className="tabItems">
         {data.map((tab, index) => (
-          <div className={`tabItem`} key={index}>
+          <div
+            className={`tabItem ${selectTab === index ? "active" : ""}`}
+            key={index}
+            onClick={() => activeTab(tab, index)}
+          >
             {tab}
           </div>
         ))}
+        <div className="movingBg" style={{ left }}></div>
       </div>
     </div>
   );
