11bd11a504598b7d432b66fa1786cd80afae51c2
abdurrahim220 <z.abdurrahim5@gmail.com>
2023-11-26 01:00:32 +0600

    trending items done

--- src/Pages/Home/trending/Trending.jsx
+++ src/Pages/Home/trending/Trending.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 import useFetch from "../../../hooks/useFetch";
+import Carousel from "../../../components/carousel/Carousel";
 
 const Trending = () => {
   const [endPoint,setEndPoint]=useState("day");
@@ -15,6 +16,7 @@ const Trending = () => {
         <div className="carouselTitle">Trending</div>
         <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
       </ContentWrapper>
+      <Carousel data={data?.results} loading={loading}/>
     </div>
   );
 };
---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,80 @@
+import React, { useRef } from "react";
+import {
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
+} from "react-icons/bs";
+import "./Carousel.scss";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import posterFallback from "../../assets/no-poster.png";
+import Img from "../lazyLoadImage/Img";
+import CircleRating from "../CircleRating/CircleRating";
+import dayjs from "dayjs";
+const Carousel = ({ data, loading }) => {
+  const carouselContainer = useRef();
+  const { url } = useSelector((state) => state.home);
+  const navigate = useNavigate();
+
+  const navigation = (dir) => {};
+  const skItem = () => {
+    return (
+      <div className="skeletonItem">
+        <div className="posterBlock skeleton"></div>
+        <div className="textBlock">
+          <div className="title skeleton"></div>
+          <div className="date skeleton"></div>
+        </div>
+      </div>
+    );
+  };
+  return (
+    <div className="carousel">
+      <ContentWrapper>
+        <BsFillArrowLeftCircleFill
+          className="carouselLeftNav arrow"
+          onClick={() => navigation("left")}
+        />
+        <BsFillArrowRightCircleFill
+          className="carouselRightNav arrow"
+          onClick={() => navigation("right")}
+        />
+        {!loading ? (
+          <div className="carouselItems">
+            {data?.map((item) => {
+              const posterUrl = item.poster_path
+                ? url.poster + item.poster_path
+                : posterFallback;
+              return (
+                <div key={item.id} className="carouselItem">
+                  <div className="posterBlock">
+                    <Img src={posterUrl} />
+                    <CircleRating rating={item.vote_average.toFixed(1)} />
+                  </div>
+                  <div className="textBlock">
+                    <span className="title">{item.title || item.name}</span>
+                    <span className="date">
+                      {dayjs(item.release_date || item.first_air_date).format(
+                        "MMM D, YYYY"
+                      )}
+                    </span>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div className="loadingSkeleton">
+            {skItem()}
+            {skItem()}
+            {skItem()}
+            {skItem()}
+            {skItem()}
+          </div>
+        )}
+      </ContentWrapper>
+    </div>
+  );
+};
+
+export default Carousel;
