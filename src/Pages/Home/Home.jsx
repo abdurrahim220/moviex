00669536f497c5dbc32e21c598c25a2693e01039
abdurrahim220 <z.abdurrahim5@gmail.com>
2023-11-26 17:05:11 +0600

    top rated section created

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,12 +3,14 @@ import './Home.scss'
 import HeroBanner from './heroBanner/HeroBanner'
 import Trending from './trending/Trending'
 import Popular from './popular/Popular'
+import TopRated from './topRated/TopRated'
 const Home = () => {
   return (
     <div className='homePage'>
       <HeroBanner/>
       <Trending/>
       <Popular/>
+      <TopRated/>
     </div>
   )
 }
